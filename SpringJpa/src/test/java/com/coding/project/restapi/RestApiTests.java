3c38d4e692d98f0598624ef381c0a6a982b08698
Rohiteswar Gouthu <21131A4209@gvpce.ac.in>
2025-12-12 18:34:22 +0530

    Basic Spring Boot APP

---
+++ SpringJpa/src/test/java/com/coding/project/restapi/RestApiTests.java
@@ -0,0 +1,13 @@
+package com.coding.project.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
